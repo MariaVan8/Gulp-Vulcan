b1d6c10e1621e71b8354986756afd85db6786a41
Maria Revelo <fer_nandita_8@hotmail.com>
2024-01-22 21:39:19 -0800

    Added banner and carousel

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -6,3 +6,22 @@ function toggleMenu() {
     var navMenu = document.getElementById('navMenu');
     navMenu.classList.toggle('show');
 }
+
+//Header carousel
+document.addEventListener('DOMContentLoaded', function() {
+    var headerElement = document.querySelector('.header'); 
+    var images = [
+      './images/header-slider-img-01.jpg',
+      './images/header-slider-img-02.jpg',
+      './images/header-slider-img-03.jpg'
+    ];
+    var currentImageIndex = 0;
+  
+    function changeBackgroundImage() {
+      headerElement.style.backgroundImage = 'url(' + images[currentImageIndex] + ')';
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+  
+    setInterval(changeBackgroundImage, 3000); 
+  });
+  
