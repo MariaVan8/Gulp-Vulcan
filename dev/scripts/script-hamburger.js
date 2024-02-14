cca1e495fc60dfd298821f491e9ce7f3e9d5fdf2
MariaVan8 <fer_nandita_8@hotmail.com>
2024-02-13 22:44:12 -0800

    updated mobile hamburger menu and animation

--- dev/scripts/script-hamburger.js
+++ dev/scripts/script-hamburger.js
@@ -1,37 +1,11 @@
 "use strict";
 
-const body = document.body;
-const bar = document.getElementById('bar');
-const nav = document.getElementById('main-navigation');
-const main = document.getElementById("main");
-var navMenu = document.getElementById('navMenu');
+let hamburger = document.querySelector('.hamburger');
+let nav = document.querySelector('.header-nav');
 
-navMenu.addEventListener("click", openMenu);
-// Prevents the focus state from activating
-navMenu.addEventListener("mousedown", function (e) {
-    e.preventDefault();
+hamburger.addEventListener('click', function() {
+    this.classList.toggle('active');
+    nav.classList.toggle('show');
 });
 
-function openMenu() {
-    body.classList.toggle("show");
-    body.classList.add("animating");
-    navMenu.classList.toggle("activated");
-    navMenu.classList.toggle("show");
-}
 
-const mql = window.matchMedia("(min-width: 560px)");
-
-mql.addListener(removeTransition);
-
-function removeTransition(e) {
-    if (e.matches) {
-        body.classList.remove("show", "animating");
-        navMenu.classList.remove("activated", "show");
-    }
-}
-
-body.addEventListener("transitionend", function () {
-    if (!body.classList.contains("show")) {
-        body.classList.remove("animating");
-    }
-});
